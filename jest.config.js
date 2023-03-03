450a25a4194d6c0219dd1086230bb12b5841a234
Fabio <fabiodn.798@gmail.com>
2023-03-03 17:01:35 +0100

    🎉 Initial commit

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['dist'],
+  resolver: 'jest-ts-webcompat-resolver',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    'index.ts',
+    'app.ts',
+    'router',
+    'error.ts',
+    'db.connect',
+    'config',
+    'model',
+    'auth.ts',
+    'authorization.ts'
+  ],
+};
